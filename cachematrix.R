3c835782d614ed2ffd62bbde3cced14d58f85c02
Benjamin <buminsky@rrcc.lacounty.gov>
2014-04-23 14:26:56 -0700

    updating Cache.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work hand in hand. The first 
+## function will creat a matrix from the arguments and 
+## store the inverse of that matrix in the global environment
+## The second function then checks for the "cached" special matrix in 
+## global enviornment and if it isn't there it computes the inverse
+## of the special matrix
 
-## Write a short comment describing this function
+## Function #1 creates the special matrix, computes the
+## inverse, and stores it in the global environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  z<-matrix(1:x^2,nrow=x)
+  makecacheinverse<<-ginv(z) ## <<- stores makecaheinverse in the global environment
 }
 
 
-## Write a short comment describing this function
+## Function #2 checks the global environment for the cached inverse of the matrix
+## if it does not exist, then it computes the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(!is.null(makecacheinverse)) {           #if there is a cache
+    message("getting cached data") 
+    return(makecacheinverse)                #just return the cache, no computation needed
+  }
+  z<-matrix(1:x^2,nrow=x)
+  makecacheinverse<<-ginv(z)
+  return(makecacheinverse)
+  
+  ## Return a matrix that is the inverse of 'z'
 }
