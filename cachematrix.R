f0f6235b2a0ecba1a1d2610b073c7da2f3609bf5
benuminsky <buminsky@rrcc.lacounty.gov>
2014-04-28 08:16:28 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,41 @@
 ## These two functions work hand in hand. The first 
-## function will creat a matrix from the arguments and 
-## store the inverse of that matrix in the global environment
+## function will create a matrix from the arguments and a list of 
+## functions (like "get", "set",etc) and store them for later use 
 ## The second function then checks for the "cached" special matrix in 
 ## global enviornment and if it isn't there it computes the inverse
-## of the special matrix
+## of the special matrix.
 
-## Function #1 creates the special matrix, computes the
-## inverse, and stores it in the global environment
+## Function #1 creates the special matrix, and a list of functions 
+## that allow for retrieval of this special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  z<-matrix(1:x^2,nrow=x)
-  makecacheinverse<<-ginv(z) ## <<- stores makecaheinverse in the global environment
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x 
+  setinverse <- function(ginv) m <<- ginv
+  getinverse <- function() m 
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+} ## <<- stores makeCaheinverse for later use if needed
 }
 
 
-## Function #2 checks the global environment for the cached inverse of the matrix
+## Function #2 checks  for the cached inverse of the matrix
 ## if it does not exist, then it computes the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-  if(!is.null(makecacheinverse)) {           #if there is a cache
-    message("getting cached data") 
-    return(makecacheinverse)                #just return the cache, no computation needed
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
   }
-  z<-matrix(1:x^2,nrow=x)
-  makecacheinverse<<-ginv(z)
-  return(makecacheinverse)
-  
-  ## Return a matrix that is the inverse of 'z'
+  data <- x$get()
+  m <- ginv(data, ...)
+  x$setinverse(m)
+  m  ## Returns a matrix that is the inverse of 'x'
+}
 }
